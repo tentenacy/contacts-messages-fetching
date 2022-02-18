ce27126c9e50c47214039ebf20b41b65b44235a5
Wonseok Lee <tentenacy@gmail.com>
2022-02-19 00:56:42 +0900

    feat: parse regex

--- app/src/main/java/com/tenutz/contactsmessagesfetching/MmsReceiver.kt
+++ app/src/main/java/com/tenutz/contactsmessagesfetching/MmsReceiver.kt
@@ -3,15 +3,11 @@ package com.tenutz.contactsmessagesfetching
 import android.annotation.SuppressLint
 import android.content.Intent
 
-import android.os.Bundle
-
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.telephony.SmsMessage
 import android.util.Log
 import android.text.TextUtils
 
-import android.content.ContentResolver
 import android.database.Cursor
 import android.net.Uri
 import android.os.Handler
@@ -49,6 +45,69 @@ class MmsReceiver : BroadcastReceiver() {
             val number = parseNumber(id)
             val msg = parseMessage(id)
             Log.i("MMSReceiver.java | parseMMS", "|$number|$msg")
+
+//message example
+/*[Web발신]
+야놀자 당일예약
+<대실>
+서울 무인호텔
+2105021850012345
+
+준특실(주차문의)
+30,000원
+
+홍길동 / 050712345678
+2021-05-02(일) 19:00~
+2021-05-02(일) 23:00 (4시간)
+도보방문*/
+            val regex = """야놀자 (?:당일|미리)예약[\r\n]+<(대실|숙박|연박)>[\r\n]+([^\r\n]+)[\r\n]+([0-9]+)[\r\n]+([^\(]+)\([^\)]+\)[\r\n]+([0-9,]+)원[\r\n]+([^ ]+) / ([0-9]+)[\r\n]+([0-9]{4}-[0-9]{2}-[0-9]{2})\([월화수목금토일]\) ([0-9]{2}:[0-9]{2})~[\r\n]+([0-9]{4}-[0-9]{2}-[0-9]{2})\([월화수목금토일]\) ([0-9]{2}:[0-9]{2}) \([0-9](?:시간|박)\)[\r\n]+(도보방문|차량방문)""".toRegex()
+
+            msg?.let { it1 ->
+                val matchResult = regex.find(it1)
+                val groupValues = matchResult?.groupValues
+                if(!groupValues.isNullOrEmpty()) {
+                    Log.d("MmsReceiver", "$groupValues")
+
+                    val reservationType = groupValues[1]
+                    Log.d("MmsReceiver", "reservationType : $reservationType")
+
+                    val store = groupValues[2]
+                    Log.d("MmsReceiver", "store: $store")
+
+                    val reservationNo = groupValues[3]
+                    Log.d("MmsReceiver", "reservationNo: $reservationNo")
+
+                    val roomType = groupValues[4]
+                    Log.d("MmsReceiver", "roomType: $roomType")
+
+                    val price = groupValues[5]
+                    Log.d("MmsReceiver", "price: $price")
+
+                    val reserveerName = groupValues[6]
+                    Log.d("MmsReceiver", "reserveerName: $reserveerName")
+
+                    val reserveerContact = groupValues[7]
+                    Log.d("MmsReceiver", "reserveerContact: $reserveerContact")
+
+                    val enterDate = groupValues[8]
+                    Log.d("MmsReceiver", "enterDate: $enterDate")
+
+                    val enterTime = groupValues[9]
+                    Log.d("MmsReceiver", "enterTime: $enterTime")
+
+                    val checkoutDate = groupValues[10]
+                    Log.d("MmsReceiver", "checkoutDate: $checkoutDate")
+
+                    val checkoutTime = groupValues[11]
+                    Log.d("MmsReceiver", "checkoutTime: $checkoutTime")
+
+                    val transportationType = groupValues[12]
+                    Log.d("MmsReceiver", "transportationType: $transportationType")
+
+                }
+
+            }
+
         }
     }
 
